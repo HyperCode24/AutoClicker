7bfc7888993fd8d443fe9e7a37328e1642fb24bf
HyperCube24 <hypercube24official@gmail.com>
2026-07-27 13:40:40 -0700

    Added support for subtitle detection and added some helpful debug information

--- Main.py
+++ Main.py
@@ -1,9 +1,23 @@
+# Import everything
 import time
-
 import pydirectinput
 import keyboard
 import ctypes
 import pynput
+import cv2
+import mss
+import numpy as np
+
+# Set up image detection and global variables (ChatGPT assistance)
+region = {
+    "left": 2270,
+    "top": 1270,
+    "width": 290,
+    "height": 73
+}
+template = cv2.imread("text screenshot.png", cv2.IMREAD_GRAYSCALE)
+template_width = template.shape[1]
+template_height = template.shape[0]
 
 # Sets the failsafe delay while keeping the corner breakout
 def setupFailsafes():
@@ -18,6 +32,32 @@ def waitForKey(key):
     while keyboard.is_pressed(key):
         time.sleep(0.1)
     print(key + " key pressed and released.")
+
+# Detect when a fish is caught and reel in automatically (ChatGPT assistance)
+def fishingSubtitle(tolerance):
+    with mss.mss() as sct:
+        screenshot = np.array(sct.grab(region))
+    gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+    result = cv2.matchTemplate(
+        gray,
+        template,
+        cv2.TM_CCOEFF_NORMED
+    )
+    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+    print(max_val)
+    if max_val > tolerance:
+        return True
+    else:
+        return False
+
+# Waits until a key is pressed and then released
+def waitForSubtitle(key):
+    print("Waiting for subtitle disappearance and then appearance.")
+    while fishingSubtitle(0.9):
+        time.sleep(0.1)
+    while not(fishingSubtitle(0.9)):
+        time.sleep(0.1)
+    print("Subtitle appeared.")
 
 # Define the delay your computer needs to register different keys here
 def stdDelay():
@@ -41,6 +81,7 @@ def globalMousePos():
     print("Position is: " + str(mousePos))
     return mousePos
 
+# Move the mouse to any monitor
 def globalMouseMove(pos):
     '''ctypes.windll.user32.SetCursorPos(pos)'''
     mouse = pynput.mouse.Controller()
@@ -49,7 +90,7 @@ def globalMouseMove(pos):
 
 # Automatically switches screens to allow multitasking while fishing
 def ac_Fishing():
-    waitForKey('v')
+    waitForSubtitle('v')
     savedSpot = globalMousePos()
     altTab()
     pydirectinput.rightClick()
@@ -62,7 +103,13 @@ def ac_Fishing():
 # Makes sure failsafes are active
 setupFailsafes()
 
-# Run the autocliker infinitely
+# Run the autocliker infinitely after a short delay for setup
+waitsec = 10
+cursec = 0
+while cursec < waitsec:
+    print("Starting in " + str(waitsec-cursec) + " seconds.")
+    cursec = cursec + 1
+    time.sleep(1)
+print("Active")
 while True:
     ac_Fishing()
-
